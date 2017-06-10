54010578a9073ca82a1f14b66db3bf8c568854ef
Serkan Hosca <serkan@hosca.com>
2017-06-09 23:33:44 -0400

    Version bump

--- rest_witchcraft/__version__.py
+++ rest_witchcraft/__version__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 __author__ = 'Serkan Hosca'
 __author_email__ = 'serkan@hosca.com'
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 __description__ = 'SQLAlchemy specific things for django-rest-framework'
