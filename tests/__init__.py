648e66b82ea77cdf454c3117cc488d954fa17006
Miroslav Shubernetskiy <miroslav@miki725.com>
2019-06-23 22:12:33 -0400

    switching to tox-travis and tox matrix (#43)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+from psycopg2cffi import compat
+
 import django
 import django.test.utils
 from django.conf import settings
 
+
+compat.register()
 
 settings.configure()
 
