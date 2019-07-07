7111a6114a2c4f193330b6cde465f71b420e740f
Serkan Hosca <serkan@hosca.com>
2019-07-07 18:44:32 -0400

    Add SearchFilter

---
+++ tests/settings.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+import os
+
+
+INSTALLED_APPS = ["rest_witchcraft", "rest_framework", "django.contrib.staticfiles"]
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+USE_THOUSAND_SEPARATOR = True
+
+SECRET_KEY = "secret"
+
+ALLOWED_HOSTS = ["*"]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(os.path.dirname(__file__), "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+            ]
+        },
+    }
+]
---
+++ tests/test_filters.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+import coreschema
+
+from django.test import RequestFactory
+
+from rest_framework.settings import api_settings
+from rest_framework.test import APISimpleTestCase
+
+from rest_witchcraft.filters import SearchFilter
+from rest_witchcraft.serializers import ModelSerializer
+from rest_witchcraft.viewsets import ModelViewSet
+
+from .models import Owner, session
+
+
+class OwnerSerializer(ModelSerializer):
+    class Meta:
+        model = Owner
+        session = session
+        fields = "first_name", "last_name"
+
+
+class OwnerViewSet(ModelViewSet):
+    serializer_class = OwnerSerializer
+    session = session
+    queryset = property(lambda self: session.query(Owner))
+
+
+class TestSearchFilters(APISimpleTestCase):
+    factory = RequestFactory()
+
+    def setUp(self):
+        super(TestSearchFilters, self).setUp()
+        self.viewset_class = OwnerViewSet
+        self.filter = SearchFilter()
+
+        self.owner1 = Owner(id=1, first_name="Joe", last_name="Smith")
+        self.owner2 = Owner(id=2, first_name="Jon", last_name="Snow")
+
+        self.owners = [self.owner1, self.owner2]
+        session.add_all(self.owners)
+        session.flush()
+
+    def tearDown(self):
+        session.rollback()
+        super(TestSearchFilters, self).tearDown()
+
+    def test_search_field(self):
+        viewset = self.viewset_class()
+        viewset.action_map = {"get": "list"}
+
+        schema = self.filter.get_schema_fields(viewset)
+
+        self.assertEqual(len(schema), 1)
+        field = schema[0]
+
+        self.assertEqual(field.name, api_settings.SEARCH_PARAM)
+        self.assertFalse(field.required)
+        self.assertEqual(field.location, "query")
+        self.assertIsInstance(field.schema, coreschema.String)
+
+        schema_ops = self.filter.get_schema_operation_parameters(viewset)
+        self.assertDictEqual(
+            {"test": schema_ops},
+            {
+                "test": [
+                    {
+                        "name": "search",
+                        "required": False,
+                        "in": "query",
+                        "description": "A search term.",
+                        "schema": {"type": "string"},
+                    }
+                ]
+            },
+        )
+
+    def test_to_html(self):
+        viewset = self.viewset_class()
+        viewset.action_map = {"get": "list"}
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "jo"}))
+
+        html = self.filter.to_html(request, viewset.get_queryset(), viewset)
+
+        self.assertInHTML("<h2>Search</h2>", html)
+
+        del viewset.__class__.search_fields
+        html = self.filter.to_html(request, viewset.get_queryset(), viewset)
+        self.assertEqual(html, "")
+
+    def test_icontains(self):
+        self.viewset_class.search_fields = ["first_name", "last_name"]
+
+        viewset = self.viewset_class()
+        viewset.action_map = {"get": "list"}
+        request = viewset.initialize_request(self.factory.get("/"))
+
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+
+        self.assertEqual(set(query.all()), set(self.owners))
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "jon"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+
+        self.assertEqual(set(query.all()), {self.owner2})
+
+    def test_istartswith(self):
+        self.viewset_class.search_fields = ["^first_name", "^last_name"]
+        viewset = self.viewset_class()
+        viewset.action_map = {"get": "list"}
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "jo"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+        self.assertEqual(set(query.all()), set(self.owners))
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "sno"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+        self.assertEqual(set(query.all()), {self.owner2})
+
+    def test_iexact(self):
+        self.viewset_class.search_fields = ["=first_name", "=last_name"]
+        viewset = self.viewset_class()
+        viewset.action_map = {"get": "list"}
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "sno"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+        self.assertEqual(set(query.all()), set())
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "snow"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+        self.assertEqual(set(query.all()), {self.owner2})
+
+    def test_exact(self):
+        self.viewset_class.search_fields = ["@first_name", "@last_name"]
+        viewset = self.viewset_class()
+        viewset.action_map = {"get": "list"}
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "snow"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+        self.assertEqual(set(query.all()), set())
+
+        request = viewset.initialize_request(self.factory.get("/", {api_settings.SEARCH_PARAM: "Snow"}))
+        query = self.filter.filter_queryset(request, viewset.get_queryset(), viewset)
+        self.assertEqual(set(query.all()), {self.owner2})
